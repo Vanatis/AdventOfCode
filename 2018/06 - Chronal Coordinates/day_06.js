5f713a5941e245a9f53373620b2635160ece6318
Gremorie <vdwcedric@gmail.com>
2018-12-07 01:09:36 +0100

    Day06 parrt 1 added

--- 2018/06 - Chronal Coordinates/day_06.js
+++ 2018/06 - Chronal Coordinates/day_06.js
@@ -12,43 +12,35 @@ function question1() {
   });
 
   lineReader.on('close', () => {
-    console.log(inputCoordinates);
-
     //Get min/max of x and y value to determine which coordinates are not infinite
-    inputCoordinates.sort((a, b) => {
-      return b.x - a.x;
+
+    let minX = Number.MAX_SAFE_INTEGER;
+    let maxX = Number.MIN_SAFE_INTEGER;
+    let minY = Number.MAX_SAFE_INTEGER;
+    let maxY = Number.MIN_SAFE_INTEGER;
+
+    inputCoordinates.map((coordinate) => {
+      minX = minX < coordinate.x ? minX : coordinate.x;
+      minY = minY < coordinate.y ? minY : coordinate.y;
+      maxX = maxX > coordinate.x ? maxX : coordinate.x;
+      maxY = maxY > coordinate.y ? maxY : coordinate.y;
     });
-    const minX = inputCoordinates[inputCoordinates.length - 1].x;
-    const maxX = inputCoordinates[0].x;
-
-    console.log("minX:", minX);
-    console.log("maxX:", maxX);
-
-    inputCoordinates.sort((a, b) => {
-      return b.y - a.y;
-    });
-    const minY = inputCoordinates[inputCoordinates.length - 1].y;
-    const maxY = inputCoordinates[0].y;
-
-    console.log("minY:", minY);
-    console.log("maxY:", maxY);
 
     //for each point on the outside of the square grid, determine closest point.
     //If a inputCoordinate is closest, that inputCoordinate has an infinite area
-    let finiteCoordinates = inputCoordinates;
-    for (let i = minX; i < maxX; ++i) {
-      //Top border
-      let minDistance = calcManhattanDistance(inputCoordinate[0], {x: i, y: minY});
-      for (let j = 1; j < inputCoordinate.length; ++j) {
-        const currentDistance = calcManhattanDistance(inputCoordinate[j], {x: i, y: minY});
-        if (minDistance > currentDistance) {
-          minDistance == currentDistance;
-        }
+    let finiteIndices = checkInfinites(inputCoordinates, minX, maxX, minY, maxY);
+    let surfaceAreas = calcSurfaceAreas(inputCoordinates, minX, maxX, minY, maxY);
+
+    let filteredAreas = surfaceAreas.map((area) => {
+      if (finiteIndices.includes(area["index"])) {
+        return area;
       }
-    }
+    })
+    filteredAreas.sort((a,b) => {
+      return b.size - a.size;
+    });
 
-
-    console.log('Question 1:');
+    console.log('Question 1:', filteredAreas[0]["size"]);
   });
 }
 
@@ -66,6 +58,104 @@ function question2() {
 
 function calcManhattanDistance(a, b) {
   return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+}
+
+function checkInfinites(inputCoordinates, minX, maxX, minY, maxY) {
+  //Horizontal edges
+  let infiniteIndices = [];
+
+  for (let x = minX - 1; x <= maxX + 1; ++x) {
+    let minDistance = Number.MAX_SAFE_INTEGER;
+    let minIndex = -1;
+    let maxDistance = Number.MAX_SAFE_INTEGER;
+    let maxIndex = -1;
+
+    for (let i = 0; i < inputCoordinates.length; ++i) {
+      let curMinDistance = calcManhattanDistance(inputCoordinates[i], {x: x, y: minY});
+      if (minDistance > curMinDistance) {
+        minDistance = curMinDistance;
+        minIndex = i;
+      }
+
+      let curMaxDistance = calcManhattanDistance(inputCoordinates[i], {x: x, y: maxY})
+      if (maxDistance > curMaxDistance) {
+        maxDistance = curMaxDistance;
+        maxIndex = i;
+      }
+    }
+    if (minIndex !== -1 && !infiniteIndices.includes(minIndex)) { infiniteIndices.push(minIndex); }
+    if (maxIndex !== -1 && !infiniteIndices.includes(maxIndex)) { infiniteIndices.push(maxIndex); }
+  }
+
+  //Vertical edges
+  for (let y = minY - 1; y <= maxY + 1; ++y) {
+    let minDistance = Number.MAX_SAFE_INTEGER;
+    let minIndex = -1;
+    let maxDistance = Number.MAX_SAFE_INTEGER;
+    let maxIndex = -1;
+
+    for (let i = 0; i < inputCoordinates.length; ++i) {
+      let curMinDistance = calcManhattanDistance(inputCoordinates[i], {x: minY, y: y});
+      if (minDistance > curMinDistance) {
+        minDistance = curMinDistance;
+        minIndex = i;
+      }
+
+      let curMaxDistance = calcManhattanDistance(inputCoordinates[i], {x: maxY, y: y})
+      if (maxDistance > curMaxDistance) {
+        maxDistance = curMaxDistance;
+        maxIndex = i;
+      }
+    }
+
+    if (minIndex !== -1 && !infiniteIndices.includes(minIndex)) { infiniteIndices.push(minIndex); }
+    if (maxIndex !== -1 && !infiniteIndices.includes(maxIndex)) { infiniteIndices.push(maxIndex); }
+  }
+
+  let finiteCoordinates = [];
+  inputCoordinates.map((coordinate, index) => {
+    if (!infiniteIndices.includes(index)) {
+        finiteCoordinates.push(index);
+      }
+    return coordinate;
+  });
+  return finiteCoordinates;
+}
+
+function calcSurfaceAreas(inputCoordinates, minX, maxX, minY, maxY) {
+  let surfaceAreas = [];
+
+  for (let x = minX; x <= maxX; ++x) {
+    for (let y = minY; y <= maxY; ++y) {
+      let shortestDistance = Number.MAX_SAFE_INTEGER;
+      let index = -1;
+      let bMultipleShortestDistances = false;
+
+      for (let i = 0; i < inputCoordinates.length; ++i) {
+        const currentDistance = calcManhattanDistance(inputCoordinates[i], {x: x, y: y});
+        if (shortestDistance > currentDistance) {
+          shortestDistance = currentDistance;
+          index = i;
+          bMultipleShortestDistances = false;
+        } else if (shortestDistance === currentDistance) {
+          index = -1;
+          bMultipleShortestDistances = true;
+        }
+      }
+
+      //Check if surfaceAreas already contains entry for current index. if not then add, if yes then increment
+      if (!bMultipleShortestDistances) {
+        let currentSurfaceArea = surfaceAreas.find((obj) => { return obj["index"] === index});
+        if (!currentSurfaceArea) {
+          surfaceAreas.push({index: index, size: 1});
+        } else {
+          ++currentSurfaceArea["size"];
+        }
+      }
+    }
+  }
+
+  return surfaceAreas;
 }
 
 question1();
